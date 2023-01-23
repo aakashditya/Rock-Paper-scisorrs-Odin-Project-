4330a98e73ddddd6803b31fc7d676f7ab6573386
aakashditya <aakashditya@gmail.com>
2023-01-23 11:41:56 +0530

    ah.. this modification is the working code , its working properly  , First iteration

--- Rock_paper_scissors.js
+++ Rock_paper_scissors.js
@@ -10,6 +10,7 @@ function getComputerChoice() {
     let choice = ["Rock", "Paper", "Scissors"];
     randomChoice = choice[Math.floor(Math.random() * choice.length)] ; //What this line does is Math random only returns value between 0-1 and then so we need to multiply witrh length , here it is 3 
     console.log(randomChoice);
+    return randomChoice;
 
 }//yahoo first part completed succesfully
 
@@ -17,6 +18,9 @@ function getComputerChoice() {
 
 function playRound(playerSelection,computerSelection) {
     //Okay here i will compare and then declare the logic of wins and loses and ties 
+    p1 = playerSelection.toLowerCase();
+    p2 = computerSelection.toLowerCase();
+    console.log(p1,p2);
     if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors" || playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock" || playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper"){
         
          console.log((`YOU Win ${playerSelection} beats ${computerSelection}`));
@@ -24,12 +28,15 @@ function playRound(playerSelection,computerSelection) {
     }
     else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper" || playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors" || playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock"){
         
-        console.log((`YOU lOSE ${computerSelection} beats ${playerSelection} `));
+        console.log((`YOU LOSE ${computerSelection} beats ${playerSelection} `));
         return 1;
     }
-    else {
-         console.log("Tied");
+    else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "rock" || playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "paper" || playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "scissors"){
+         console.log(" Ohh Its the Same ,Tied");
          return 2;
+    }
+    else {
+        console.log("there might be some error in your input or code");
     }
    
 
@@ -38,12 +45,17 @@ function game() {
     let counter1 = 0;
     let counter2 = 0;
     for(let i = 0; i <5 ; i++){
-        playerSelection = prompt("Enter Rock Or Paper or Scissors");
-        console.log(playRound(playerSelection,getComputerChoice()));
-        if(playRound(playerSelection,getComputerChoice )== 0){
+        if (i == 4) {
+            console.log('Last one');
+        }
+        let playerSelection = window.prompt("Choose --> ROCK PAPER SCISSORS!");
+        let compuCho = getComputerChoice();
+        let play = playRound(playerSelection,compuCho);
+       // console.log(playRound(playerSelection,compuCho));
+        if(play == 0){
             counter1++;
         }
-        else if(playRound(playerSelection,getComputerChoice())== 1){
+        else if(play == 1){
             counter2++;
         }
         else {
@@ -53,16 +65,16 @@ function game() {
 
     }
     if(counter1 >counter2){
-        console.log('YOU Win The Game!');
+        console.log(`YOU Win The Game!by ${counter2 - counter1} points`);
 
     }
     else if(counter2 >counter1){
-        console.log('Computer wins the Game');
+        console.log(`The Computer wins the Game by ${counter2 -counter1} points`);
     }
     else{
-        console.log("game is Tied");
+        alert("The Game is Tied. Play Again");
     }
 
 }
 
-console.log(playRound());
+
